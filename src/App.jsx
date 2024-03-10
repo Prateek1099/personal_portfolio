140fa99d11cc30bb651971e26455a6cd4c1f5067
prateekparihar10 <businessfacts1099@gmail.com>
2024-03-10 18:21:59 +0530

    photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/hero/Hero";
 import Test from "./Test";
 
 import "./app.scss"
 const App = () => {
   return <div>
 
-    <section>
+    <section id="Homepage">
       <Navbar />
+      <Hero />
     </section>
-    <section>Parallax</section>
+    <section id="Services">Parallax</section>
     <section>Services</section>
-    <section>Parallax</section>
+    <section id="Portfolio">Parallax</section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
-    <section>Contact</section> 
+
+    <section id="Contact">Contact</section> 
     {/* <Test /> */}
 
   </div>;
---
+++ src/Components/hero/Hero.jsx
@@ -0,0 +1,16 @@
+import "./hero.scss"
+
+const Hero =()=>{
+    return(
+        <>
+        <div className="hero">
+            <div className="imageContainer">
+               <img src="hero.png" /> 
+            </div>
+        
+        </div>
+        </>
+    )
+
+}
+export default Hero;
